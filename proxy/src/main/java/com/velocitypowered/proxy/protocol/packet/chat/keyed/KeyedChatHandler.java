071dcd826788dfa334e0408b11e6f77eda08bea4
Jacob Sapoznikow <jacob1coder@gmail.com>
2023-03-31 19:18:16 -0700

    Hopefully patch

--- proxy/src/main/java/com/velocitypowered/proxy/protocol/packet/chat/keyed/KeyedChatHandler.java
+++ proxy/src/main/java/com/velocitypowered/proxy/protocol/packet/chat/keyed/KeyedChatHandler.java
@@ -115,7 +115,7 @@ public class KeyedChatHandler implements
       if (chatResult.getMessage().map(str -> !str.equals(packet.getMessage())).orElse(false)) {
         if (playerKey.getKeyRevision().compareTo(IdentifiedKey.Revision.LINKED_V2) >= 0) {
           // Bad, very bad.
-          invalidChange(logger, player);
+          // invalidChange(logger, player);
         } else {
           logger.warn("A plugin changed a signed chat message. The server may not accept it.");
           return player.getChatBuilderFactory().builder()
