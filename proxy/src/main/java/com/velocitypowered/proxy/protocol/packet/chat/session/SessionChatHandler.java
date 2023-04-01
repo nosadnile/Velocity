2d84ce3e9473402eb6354f2d3fbe0e3c29aefac1
Jacob Sapoznikow <jacob1coder@gmail.com>
2023-03-31 19:26:21 -0700

    Yet another patch

--- proxy/src/main/java/com/velocitypowered/proxy/protocol/packet/chat/session/SessionChatHandler.java
+++ proxy/src/main/java/com/velocitypowered/proxy/protocol/packet/chat/session/SessionChatHandler.java
@@ -17,8 +17,6 @@
 
 package com.velocitypowered.proxy.protocol.packet.chat.session;
 
-import static com.velocitypowered.proxy.protocol.packet.chat.keyed.KeyedChatHandler.invalidCancel;
-import static com.velocitypowered.proxy.protocol.packet.chat.keyed.KeyedChatHandler.invalidChange;
 
 import com.velocitypowered.api.event.EventManager;
 import com.velocitypowered.api.event.player.PlayerChatEvent;
@@ -56,18 +54,18 @@ public class SessionChatHandler implements ChatHandler<SessionPlayerChat> {
             .thenApply(pme -> {
               PlayerChatEvent.ChatResult chatResult = pme.getResult();
               if (!chatResult.isAllowed()) {
-                if (packet.isSigned()) {
+                // if (packet.isSigned()) {
                   // invalidCancel(logger, player);
-                }
+                // }
                 return null;
               }
 
               if (chatResult.getMessage().map(str -> !str.equals(packet.getMessage()))
                   .orElse(false)) {
-                if (packet.isSigned()) {
+                // if (packet.isSigned()) {
                   // invalidChange(logger, player);
-                  return null;
-                }
+                  // return null;
+                // }
                 return this.player.getChatBuilderFactory().builder().message(packet.message)
                     .setTimestamp(packet.timestamp)
                     .toServer();
